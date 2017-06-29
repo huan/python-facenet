58c313f94297111a712148dc111ca1d3eae02b4c
Huan LI <zixia@zixia.net>
2017-06-29 15:02:12 +0800

    add ui class

---
+++ src/test_ui.py
@@ -0,0 +1,17 @@
+""" module """
+from ui import Ui
+
+ui = Ui()
+ui.show_photo('../opencv-python-tutorial/brothers.jpg')
+ui.draw_rectangle(5, 5, 10, 10)
+ui.interactive_mode()
+ui.show()
+
+# ui.pause(1)
+
+print('draw rect')
+
+ui.draw_rectangle(50, 50, 100, 100)
+# ui.draw()
+
+ui.pause(10)
---
+++ src/ui.py
@@ -0,0 +1,83 @@
+"""
+dialog module
+"""
+from time import sleep
+from typing import Tuple
+
+import matplotlib.pyplot as plt
+# from matplotlib.image import AxesImage
+from matplotlib.patches import Rectangle
+
+import numpy as np
+
+Point = Tuple[int, int]
+
+i = 3   # type: int
+i = '3'
+
+
+class Ui(object):
+    """
+    dialog
+    """
+
+    def __init__(self):
+        self.im = None   # type: AxesImage
+        self.fig, self.ax = plt.subplots()
+
+    def show_photo(self, file: str) -> None:
+        """ show photo
+        """
+        img = plt.imread(file)
+        self.im = plt.imshow(img)
+
+    def draw_rectangle(
+            self,
+            x: int,
+            y: int,
+            width: int,
+            height: int,
+    ) -> None:
+        """ draw """
+        # color = np.random.randint(3)
+        # color = color.list()
+        rect = Rectangle(
+            [x, y],
+            width,
+            height,
+            linewidth=1,
+            edgecolor='r',
+            facecolor='none'
+        )
+        self.ax.add_patch(rect)
+
+    def draw(self) -> None:
+        """ refresh plt show """
+        # self.ax.draw()
+        plt.draw()
+
+    def show(self):
+        """ show """
+        self.ax.axis('off')  # clear x- and y-axes
+        plt.show()
+
+    def pause(self, seconds: int) -> None:
+        """ pause """
+        plt.pause(seconds)
+
+    def sleep(self, seconds: int) -> None:
+        """ sleep """
+        sleep(seconds)
+
+    def interactive_mode(self, switch: bool = True) -> None:
+        """ interactive """
+        if switch:
+            plt.ion()
+        else:
+            plt.ioff()
+
+    def huan(self, n: int) -> None:
+        """ test
+        """
+        print('huan%d' % (n))
+
