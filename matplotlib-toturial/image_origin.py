c5eb1fe16105dc1ab7ae84e9a365c5d3c57ecdeb
Huan LI <zixia@zixia.net>
2017-07-01 15:59:29 +0800

    add more matplotlib examples

---
+++ matplotlib-toturial/image_origin.py
@@ -0,0 +1,27 @@
+"""
+http://matplotlib.org/examples/pylab_examples/image_origin.html
+You can specify whether images should be plotted with the array origin
+x[0,0] in the upper left or upper right by using the origin parameter.
+You can also control the default be setting image.origin in your
+matplotlibrc file; see http://matplotlib.org/matplotlibrc
+"""
+# %%
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.arange(120)
+x.shape = (10, 12)
+# x = np.zeros((10, 12))
+# x[5:, :] += 255
+# print(x)
+
+interp = 'bilinear'
+fig, axs = plt.subplots(nrows=2, sharex=True, figsize=(3, 5))
+
+axs[0].set_title('blue should be up')
+axs[0].imshow(x, origin='upper', interpolation=interp)
+
+axs[1].set_title('blue should be down')
+axs[1].imshow(x, origin='lower', interpolation=interp)
+
+plt.show()
