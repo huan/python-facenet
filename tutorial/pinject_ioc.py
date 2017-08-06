341f5c06126956492767dd555687c5167da631e3
Huan LI <zixia@zixia.net>
2017-08-06 23:14:29 +0800

    rename ioc

--- tutorial/pinject_ioc.py
+++ tutorial/pinject_ioc.py
@@ -4,17 +4,21 @@ https://github.com/google/pinject
 """
 import pinject
 
+
 class OuterClass(object):
     # @pinject.copy_args_to_internal_fields
     @pinject.copy_args_to_public_fields
     def __init__(self, inner_class):
+        # print(inner_class)
         pass
+
 
 class InnerClass(object):
     def __init__(self):
         self.forty_two = 42
 
-obj_graph = pinject.new_object_graph()
-outer_class = obj_graph.provide(OuterClass)
-print(outer_class.inner_class.forty_two)
 
+ioc = pinject.new_object_graph()
+outer_class = ioc.provide(OuterClass)
+# outer_class = OuterClass()
+print(outer_class.inner_class.forty_two)
